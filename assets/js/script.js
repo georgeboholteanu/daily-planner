4c820068f43a5f5cb91eda729e55b4d953215a7a
georgeb <george.boholteanu@gmail.com>
2023-01-22 17:40:02 +0000

    added setBGcolor function to set table bg color dynamically and fix BS CDN link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,35 @@
+
 //fn returns curent time
 function getTime() {
     var now = moment();
     return now.format('dddd, MMMM Do - HH:mm:ss');
 }
 
-// refresh planner css every 1s
+
+//fn sets planner CSS
+function setBGcolor() {
+    var tdd = $('td[class="hour"]');
+    tdd.each(function (index) {
+        var eL = tdd[index];
+        if ($(tdd[index]).html().split(":")[0] < moment().format('HH')) {         
+            $(eL).next().addClass("past");
+        }else if ($(eL).html().split(":")[0] > moment().format('HH')) {
+            $(eL).next().addClass("future");
+        }else {
+            $(eL).next().addClass("present");
+        }
+    });
+}
+
+
+// refresh planner css and time
 setInterval(function () {
     $('#currentDay').text(getTime());
+    setBGcolor();
 }, 1000);
+
+
+
+
+
+
